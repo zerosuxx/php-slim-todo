25eb973e5be751e6474012169f525afad708116b
Mohos Tamás <tomi@mohos.name>
2018-09-05 16:32:05 +0200

    chore(Test): add TodoAppTest directory

--- test/TodoAppTest/Integration/HealthCheckPageTest.php
+++ test/TodoAppTest/Integration/HealthCheckPageTest.php
@@ -16,4 +16,16 @@ class HealthCheckPageTest extends SlimTestCase
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals('OK', (string)$response->getBody());
     }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     */
+    public function callsHealthCheckPage_WithBadConnectionData_Returns200OK()
+    {
+        $this->disableSlimErrorHandler(false);
+        putenv('DB_USER=""');
+        $response = $this->runApp('GET', '/healthcheck');
+        $this->assertEquals(500, $response->getStatusCode());
+    }
 }
