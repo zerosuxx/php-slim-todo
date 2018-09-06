a14559cd174f65edc006f88fa20b70d788df0285
Mohos Tamás <tomi@mohos.name>
2018-09-06 14:08:02 +0200

    docs(AppBuilder): add missing phpdocs

--- src/AppBuilder.php
+++ src/AppBuilder.php
@@ -15,11 +15,20 @@ class AppBuilder
      */
     private $providers = [];
 
+    /**
+     * @param callable $provider
+     * @return self
+     */
     public function addProvider(callable $provider)
     {
         $this->providers[] = $provider;
+        return $this;
     }
 
+    /**
+     * @param array $container
+     * @return App
+     */
     public function buildApp($container = []): App
     {
         $app = new App($container);
