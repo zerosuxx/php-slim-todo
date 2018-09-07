e8e7e20d4dd2f4faf6d45392cd0fed5d0d7fe87d
Mohos Tamás <tomi@mohos.name>
2018-09-07 14:50:05 +0200

    feat(TodosDao): getTodos returns todos ascendingly ordered by due date

--- test/TodoAppTest/Integration/Dao/TodosDaoTest.php
+++ test/TodoAppTest/Integration/Dao/TodosDaoTest.php
@@ -108,5 +108,28 @@ class TodosDaoTest extends TodoAppTestCase
         $this->assertEquals('incomplete', $savedRecords[1]->getStatus());
     }
 
+    /**
+     * @test
+     */
+    public function getTodos_GivenExistingRecords_ReturnsOrderedTodos()
+    {
+        $records = [
+            new Todo('name 1', 'desc 1', 'incomplete', new \DateTime('2018-09-09 10:00:00')),
+            new Todo('name 2', 'desc 2', 'incomplete', new \DateTime('2018-09-07 10:00:00')),
+            new Todo('name 3', 'desc 3', 'incomplete', new \DateTime('2018-09-08 10:00:00'))
+        ];
+
+        $savedRecords = [];
+        foreach ($records as $record) {
+            $savedRecords[] = $this->dao->saveTodo($record);
+        }
+
+        $todosFromDb = $this->dao->getTodos();
+
+        $this->assertEquals('name 2', $todosFromDb[0]->getName());
+        $this->assertEquals('name 3', $todosFromDb[1]->getName());
+        $this->assertEquals('name 1', $todosFromDb[2]->getName());
+    }
+
 
 }
