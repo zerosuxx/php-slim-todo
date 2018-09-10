a963396f798905cf3be76beb05e3b67fbc552814
Mohos Tamás <tomi@mohos.name>
2018-09-10 16:58:12 +0200

    feat(EditViewAction): handle data and errors

--- src/TodoApp/Action/EditViewAction.php
+++ src/TodoApp/Action/EditViewAction.php
@@ -5,6 +5,7 @@ namespace TodoApp\Action;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Views\Twig;
+use TodoApp\ConfigProvider;
 use TodoApp\Dao\TodosDao;
 use Zero\Form\Validator\CSRFTokenValidator;
 
@@ -36,6 +37,31 @@ class EditViewAction
     {
         $id = (int)$args['id'];
         $todo = $this->dao->getTodo($id);
-        return $this->view->render($response, 'edit.html.twig', ['action' => '/todo/edit/' . $id]);
+        $data = $this->consumeSessionData('data', []) + [
+            'name' => $todo->getName(),
+            'description' => $todo->getDescription(),
+            'due_at' => $todo->getDueAt()->format(ConfigProvider::FORMAT_DATETIME),
+        ];
+        $data['token'] = $this->csrf->getToken();
+        $errors = $this->consumeSessionData('errors', []);
+        return $this->view->render($response, 'edit.html.twig', [
+            'action' => '/todo/edit/' . $id,
+            'data' => $data,
+            'errors' => $errors
+        ]);
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $default [optional] default: null
+     * @return array|mixed
+     */
+    private function consumeSessionData(string $key, $default = null) {
+        if(!empty($_SESSION[$key])) {
+            $data = $_SESSION[$key];
+            unset($_SESSION[$key]);
+            return $data;
+        }
+        return $default;
     }
 }
--- test/TodoAppTest/Integration/EditViewActionTest.php
+++ test/TodoAppTest/Integration/EditViewActionTest.php
@@ -38,4 +38,20 @@ class EditViewActionTest extends TodoAppTestCase
         $this->expectException(\InvalidArgumentException::class);
         $this->runApp('GET', '/todo/edit/not-exists');
     }
+
+    /**
+     * @test
+     */
+    public function callsEditPage_GivenErrorsAndData_Returns200WithErrorsAndData()
+    {
+        $this->dao->saveTodo(new Todo('Test Name', 'Test message', 'incomplete', new \DateTime()));
+        $_SESSION['errors'] = ['name' => 'Invalid data'];
+        $_SESSION['data'] = ['description' => 'Test desc'];
+        $response = $this->runApp('GET', '/todo/edit/1');
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('Invalid data', (string)$response->getBody());
+        $this->assertContains('Test desc', (string)$response->getBody());
+        $this->assertArrayNotHasKey('errors', $_SESSION);
+        $this->assertArrayNotHasKey('data', $_SESSION);
+    }
 }
