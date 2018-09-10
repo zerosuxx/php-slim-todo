9450f57af3a0d426f78dea0d04b53ef6b9da5871
Mohos Tamás <tomi@mohos.name>
2018-09-10 14:21:35 +0200

    feat(IndexPage): add more assertions

--- test/TodoAppTest/Integration/IndexPageTest.php
+++ test/TodoAppTest/Integration/IndexPageTest.php
@@ -25,10 +25,14 @@ class IndexPageTest extends TodoAppTestCase
     public function callsListPage_Returns200WithTodos()
     {
         $savedTodo = $this->dao->saveTodo(new Todo('Test Name', 'test message', 'incomplete', new \DateTime()));
+        $savedTodo2 = $this->dao->saveTodo(new Todo('Test Name2', 'test message2', 'incomplete', new \DateTime()));
         $response = $this->runApp('GET', '/todos');
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertContains($savedTodo->getName(), (string)$response->getBody());
         $this->assertContains($savedTodo->getDescription(), (string)$response->getBody());
         $this->assertContains($savedTodo->getDueAt()->format('Y-m-d H:i:s'), (string)$response->getBody());
+        $this->assertContains($savedTodo2->getName(), (string)$response->getBody());
+        $this->assertContains($savedTodo2->getDescription(), (string)$response->getBody());
+        $this->assertContains($savedTodo2->getDueAt()->format('Y-m-d H:i:s'), (string)$response->getBody());
     }
 }
