fb8493d967f3ea84db95b00614b83e031ed3d887
Mohos Tamás <tomi@mohos.name>
2018-09-10 15:40:10 +0200

    feat(DeleteAction): handle not exists todo

--- test/TodoAppTest/Integration/DeleteActionTest.php
+++ test/TodoAppTest/Integration/DeleteActionTest.php
@@ -2,6 +2,8 @@
 
 namespace Test\TodoAppTest\Integration;
 
+use DateTime;
+use InvalidArgumentException;
 use Test\TodoAppTest\TodoAppTestCase;
 use TodoApp\Dao\TodosDao;
 use TodoApp\Entity\Todo;
@@ -24,12 +26,21 @@ class DeleteActionTest extends TodoAppTestCase
      */
     public function callsDeletePage_GivenValidData_Returns301()
     {
-        $this->dao->saveTodo(new Todo('Test Name', 'Test message', 'incomplete', new \DateTime()));
+        $this->dao->saveTodo(new Todo('Test Name', 'Test message', 'incomplete', new DateTime()));
         $response = $this->runApp('POST', '/todo/delete/1');
 
         $this->assertEquals(301, $response->getStatusCode());
 
         $todos = $this->dao->getTodos();
         $this->assertCount(0, $todos);
+    }
+
+    /**
+     * @test
+     */
+    public function callsDeletePage_WithNotExistsTodo_ThrowException()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->runApp('POST', '/todo/delete/not-exists');
     }
 }
