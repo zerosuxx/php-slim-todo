847943226e15f3caf7a0ea3391eaadb2e5b5950c
Mohos Tamás <tomi@mohos.name>
2018-09-10 16:24:43 +0200

    feat(TodoApp): add session start to index.php

--- public/index.php
+++ public/index.php
@@ -5,5 +5,6 @@ use Slim\App;
 require_once dirname(__DIR__) . '/config/bootstrap.php';
 
 (function(App $app) {
+    session_start();
     $app->run();
 })(require dirname(__DIR__) . '/config/app.php');
