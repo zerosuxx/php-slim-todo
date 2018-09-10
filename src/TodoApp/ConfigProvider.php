6d9346d9185b8efed0f6a6052412c9e28a5f7b2e
Mohos Tamás <tomi@mohos.name>
2018-09-10 10:53:10 +0200

    feat(TodosDao): add list route and ListAction dependencies

--- src/TodoApp/ConfigProvider.php
+++ src/TodoApp/ConfigProvider.php
@@ -5,7 +5,11 @@ namespace TodoApp;
 use PDO;
 use Psr\Container\ContainerInterface;
 use Slim\App;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
 use TodoApp\Action\HealthCheckAction;
+use TodoApp\Action\ListAction;
+use TodoApp\Dao\TodosDao;
 
 /**
  * Class ConfigProvider
@@ -26,19 +30,36 @@ class ConfigProvider
 
     public function dependencies(ContainerInterface $container)
     {
-        $container['PDO'] = function () {
+        $container['pdo'] = function () {
             $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8mb4', getenv('DB_HOST'), getenv('DB_NAME'));
             $pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASSWORD'));
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdo;
         };
+        $container['view'] = function (ContainerInterface $container) {
+            $view = new Twig(__DIR__ . '/templates', [
+                'cache' => false
+            ]);
+
+            $router = $container->get('router');
+            $uri = $container->get('request')->getUri();
+            $view->addExtension(new TwigExtension($router, $uri));
+            return $view;
+        };
+        $container[TodosDao::class] = function (ContainerInterface $container) {
+            return new TodosDao($container->get('pdo'));
+        };
         $container[HealthCheckAction::class] = function (ContainerInterface $container) {
-            return new HealthCheckAction($container->get('PDO'));
+            return new HealthCheckAction($container->get('pdo'));
+        };
+        $container[ListAction::class] = function (ContainerInterface $container) {
+            return new ListAction($container->get(TodosDao::class), $container->get('view'));
         };
     }
 
     public function routes(App $app)
     {
         $app->get('/healthcheck', HealthCheckAction::class);
+        $app->get('/list', ListAction::class);
     }
 }
