ea1d0a4ba9f68385b6521f07f4b92d2354c22c36
Mohos Tamás <tomi@mohos.name>
2018-09-12 20:29:40 +0200

    test(Storage): add more test case

--- test/TodoApp/Unit/Storage/SessionStorageTest.php
+++ test/TodoApp/Unit/Storage/SessionStorageTest.php
@@ -20,12 +20,17 @@ class SessionStorageTest extends TestCase
     {
         $_SESSION = [
             'test-key' => [
-                'test-key2' => null
+                'test-key2' => null,
+                'testKey3' => null
             ]
         ];
         $storage = new SessionStorage();
         $storage->get('test-key')['test-key2'] = 'test-value2';
+        $storage->{'test-key'}['testKey3'] = 'test-value3';
+        $storage['test-key']['testKey4'] = 'test-value4';
         $this->assertEquals('test-value2', $_SESSION['test-key']['test-key2']);
+        $this->assertEquals('test-value3', $_SESSION['test-key']['testKey3']);
+        $this->assertEquals('test-value4', $_SESSION['test-key']['testKey4']);
     }
 
     /**
@@ -33,12 +38,13 @@ class SessionStorageTest extends TestCase
      */
     public function set_WithData_EditReference()
     {
-        $_SESSION = [
-            'test-key' => null
-        ];
         $storage = new SessionStorage();
         $storage->set('test-key', 'test-value');
+        $storage->testKey2 = 'test-value2';
+        $storage['testKey3'] = 'test-value3';
         $this->assertEquals('test-value', $_SESSION['test-key']);
+        $this->assertEquals('test-value2', $_SESSION['testKey2']);
+        $this->assertEquals('test-value3', $_SESSION['testKey3']);
     }
 
     /**
@@ -47,11 +53,15 @@ class SessionStorageTest extends TestCase
     public function remove_WithData_RemoveDataFromReference()
     {
         $_SESSION = [
-            'test-key' => null
+            'test-key' => null,
+            'testKey2' => null,
+            'testKey3' => null
         ];
         $storage = new SessionStorage();
         $this->assertNotEmpty($_SESSION);
         $storage->remove('test-key');
+        unset($storage->testKey2);
+        unset($storage['testKey3']);
         $this->assertEmpty($_SESSION);
     }
 
